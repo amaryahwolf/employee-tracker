7085923a09a228c8eb0c9671c30da9e782a1bdd4
amaryahwolf <115281632+amaryahwolf@users.noreply.github.com>
2023-01-22 09:07:21 -0800

    add README

---
+++ README.md
@@ -0,0 +1,37 @@
+# Employee Tracker
+ 
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+## Description
+This application is a content management system used view, interact with, and udpate employee information using Node.js, Inquirer and MySQL. Check out this walkthrough [video](https://drive.google.com/file/d/1F6Ufics5ZlJexiTJ_I28BunVThkp_5ES/view) for a look into how the application functions.
+
+## Table of Contents
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+To install the application, first require the necessary packages by running npm i.
+
+## Usage
+To use the application, start the server by running node index.js in the command line. Then toggle through various options to view, interact with and update the employee database.
+![Screenshot of application](public/images/employee-tracker.png)
+
+## Credits
+Collaborators: Amaryah Wolf
+
+## License
+This application is covered under the MIT license.
+
+## Contributing
+N/A
+
+## Tests
+N/A
+
+## Questions
+For additional questions, find me on [github](https://github.com/amaryahwolf) or email me at amaryahwolf@gmail.com.
--- index.js
+++ index.js
@@ -26,52 +26,53 @@ const options = [
             'Add a department',
             'Add a role',
             'Add an employee',
-            'Update an employee role'
+            'Update an employee role',
+            'Exit the application'
         ],
         name: 'options',
     }
 ];
 
 // Function to prompt user and query to db
-function init() {
+async function init() {
     try {
         inquirer
             .prompt(options)
             .then(async (res) => {
                 if ('View all departments' === res.options) {
-                    db.query(`SELECT * FROM department`, function (err, results) {
-                        console.table(results);
-                        return init();
-                    });
+                    const [results] = await db.promise().query(`SELECT * FROM department`)
+                    console.table(results);
+                    return init();
+
                 } else if ('View all roles' === res.options) {
-                    db.query(`SELECT role.title, role.id, role.salary, department.name AS department_id
-            FROM  role
-            JOIN department ON role.department_id = department.id;`, function (err, results) {
-                        console.table(results);
-                        return init();
-                    });
+                    const [results] = await db.promise().query(`SELECT role.title, role.id, role.salary, department.name AS department_id
+                    FROM  role
+                    JOIN department ON role.department_id = department.id;`)
+                    console.table(results);
+                    return init();
+
                 } else if ('View all employees' === res.options) {
-                    db.query(`SELECT first_name, last_name FROM employee`, function (err, results) { //needs updated query
-                        console.table(results);
-                        return init();
-                    });
+                    const [results] = await db.promise().query(`SELECT employee.id, employee.first_name, employee.last_name, role.salary, role.title AS role, department.name AS department, CONCAT(manager.first_name, "_", manager.last_name) AS manager
+                    FROM employee
+                    JOIN role ON employee.role_id = role.id
+                    JOIN department ON role.department_id = department.id
+                    LEFT JOIN employee manager ON  manager.id = employee.manager_id;`)
+                    console.table(results);
+                    return init();
+
                 } else if ('Add a department' === res.options) {
-                    // Prompt user to input department name
-                    inquirer
-                        .prompt([{
+                    const res = await inquirer.prompt([
+                        {
                             type: 'input',
                             message: 'Enter department name:',
                             name: 'newDepartment',
-                        }])
-                        // Insert into db
-                        .then((res) => {
-                            db.query(`INSERT INTO department(name) VALUES (?)`, res.newDepartment, function (err, results) {
-                                console.table(results);
-                                return init();
-                            });
-                        });
+                        }
+                    ])
+                    const [results] = await db.promise().query(`INSERT INTO department(name) VALUES (?)`, res.newDepartment)
+                    console.table(results);
+                    return init();
+
                 } else if ('Add a role' === res.options) {
-                    // Prompt user to input title, salary, department id
                     const [departments] = await db.promise().query(`SELECT name, id AS value FROM department`)
                     const res = await inquirer.prompt([
                         {
@@ -90,27 +91,42 @@ function init() {
                             choices: departments,
                             name: 'department'
                         },
-                    ])
+                    ]);
                     const [results] = await db.promise().query(`INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)`, [res.title, res.salary, res.department])
                     console.table(results);
                     return init();
-                    //} // else if ('Add an employee' === res.options) {
-                    //     // Prompt user to input first name, last name, role id, manager id
-                    //      inquirer
-                    //      .prompt([
-                    //         {
-                    //             type: 'input',
-                    //             message: 'Enter first name:',
-                    //             name: 'firstName',
-                    //         },
-                    //         {
-                    //             type: 'input',
-                    //             message: 'Enter last name:',
-                    //             name: 'lastName',
-                    //         },
-                    // Prompt for role id
-                    // Prompt for manager
-                    //     // Insert into db
+
+                } else if ('Add an employee' === res.options) {
+                    const [role] = await db.promise().query(`SELECT title AS name, id AS value FROM role`);
+                    const [manager] = await db.promise().query(`SELECT CONCAT(first_name, "_", last_name) AS name, manager_id AS value FROM employee`); // query not working
+                    const res = await inquirer.prompt([
+                            {
+                                type: 'input',
+                                message: 'Enter first name:',
+                                name: 'firstName',
+                            },
+                            {
+                                type: 'input',
+                                message: 'Enter last name:',
+                                name: 'lastName',
+                            },
+                            {
+                                type: 'list',
+                                message: 'Select a role',
+                                choices: role,
+                                name: 'role'
+                            },
+                            {
+                                type: 'list',
+                                message: 'Select a manager',
+                                chocies: manager,
+                                name: 'manager'
+                            }
+                    ]);
+                    const [results] = await db.promise().query(`INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`, [res.first_name, res.last_name, res.role, res.manager])
+                    console.table(results);
+                    return init();
+                
                 } else if ('Update an employee role' === res.options) {
                     const [employee] = await db.promise().query(`SELECT CONCAT(first_name, "_", last_name) AS name, id AS value FROM employee`)
                     const [role] = await db.promise().query(`SELECT title AS name, id AS value FROM role`)
@@ -123,15 +139,16 @@ function init() {
                         },
                         {
                             type: 'list',
-                            message: 'Select an role:',
+                            message: 'Select a role:',
                             choices: role,
                             name: 'role'
                         }
-                    ])
-                    db.query(`UPDATE employee SET role_id = ? WHERE id = ?`, [res.role, res.employee], function (err, results) {
-                        console.table(results);
-                        return init();
-                    });
+                    ]);
+                    const [results] = await db.promise().query(`UPDATE employee SET role_id = ? WHERE id = ?`, [res.role, res.employee])
+                    console.table(results);
+                    return init();
+                } else {
+                    process.exit(0);
                 }
             })
     } catch (err) {
